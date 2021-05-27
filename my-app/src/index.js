175f1623b46b7bfb541260a741d6cf609bc3f454
eriudo <erildojunior@live.com>
2021-05-27 14:20:17 -0300

    refactor from consts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+const title = "American Marxism";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg";
+const author = "Mark R. Levin";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   );
@@ -18,25 +18,11 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <MyTitle />
-      <Author />
+      <img src={img} alt="" />
+      <h1> {title} </h1>
+      <h4> {author} </h4>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-
-const MyTitle = () => <h1> American Marxism</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Mark R. Levin
-  </h4>
-);
 ReactDom.render(<BookList />, document.getElementById("root"));
