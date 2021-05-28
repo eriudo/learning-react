6789b7686e99bddf7b102e5fb6c39616ab74313a
eriudo <erildojunior@live.com>
2021-05-28 13:54:46 -0300

    modularization and examples of events

---
+++ my-app/src/books.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg",
+    title: "American Marxism",
+    author: "Mark R. Levin",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Agreements: A Practical Guide to Personal Freedom ",
+    author: "Don Miguel Ruiz",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/713XUMrojkL._AC_UL200_SR200,200_.jpg",
+    title:
+      "What Happened to You?: Conversations on Trauma, Resilience, and Healing",
+    author: "Oprah Winfrey",
+  },
+];
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,48 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
+
+// CSS
 import "./index.css";
 
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg",
-    title: "American Marxism",
-    author: "Mark R. Levin",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg",
-    title: "The Four Agreements: A Practical Guide to Personal Freedom ",
-    author: "Don Miguel Ruiz",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/713XUMrojkL._AC_UL200_SR200,200_.jpg",
-    title:
-      "What Happened to You?: Conversations on Trauma, Resilience, and Healing",
-    author: "Oprah Winfrey",
-  },
-];
+import { data } from "./books";
+import SpecificBook from "./Book";
 
 function BookList() {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book key={book.id} book={book} />;
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1> {title} </h1>
-      <h4> {author} </h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
